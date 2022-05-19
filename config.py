bf0214a3199947c4220ec3baad694639755e55e4
volodymyr <volodymyr.kolomiiets.m@gmail.com>
2022-05-20 01:09:47 +0300

    create user model and dockerise db

--- config.py
+++ config.py
@@ -8,15 +8,23 @@ if os.path.exists(dotenv_path):
 
 
 class Config:
-    SECRET_KEY = 'O0tvhi2D67NPhHiJK6VLPw'
-    SQLALCHEMY_TRACK_MODIFICATION = False
+    SECRET_KEY = os.environ.get("SECRET_KEY")
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
 
 
 class DevelopmentConfig(Config):
+    user = os.environ.get('MYSQL_USER')
+    user_password = os.environ.get("MYSQL_PASSWORD")
+    mysl_host = os.environ.get("MYSQL_HOST")
+    mysql_port = os.environ.get("MYSQL_PORT")
+    mysql_db = os.environ.get("MYSQL_DB")
+
     ENV = 'development'
     DEBUG = True
-    SQLALCHEMY_DATABASE_URI = 'mysql+pymysql://'
+
+    SQLALCHEMY_DATABASE_URI = f'mysql+mysqlconnector://{user}:{user_password}@{mysl_host}:{mysql_port}/{mysql_db}'
     SQLALCHEMY_ECHO=True
+
 
 class ProductionConfig(Config):
     pass
